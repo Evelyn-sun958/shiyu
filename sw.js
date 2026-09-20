20b2e08ec5fa2a70c330dd9ba04d91087a96e729
shiyu-app <shiyu-app@users.noreply.github.com>
2026-09-20 12:01:05 +0800

    shiyu voice timeline PWA initial release

---
+++ sw.js
@@ -0,0 +1,42 @@
+/* 时语 Service Worker：网络优先、离线回退缓存 */
+const CACHE = 'shiyu-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return; // DeepSeek API 等跨域请求不拦截
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() =>
+        caches.match(event.request, { ignoreSearch: true }).then((hit) => hit || caches.match('./index.html'))
+      )
+  );
+});
